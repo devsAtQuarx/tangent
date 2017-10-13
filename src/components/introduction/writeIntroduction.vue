d98586af53b5ff8f802fcde45dfd2efd73897995
shivam singh <devatquarxss@gmail.com>
2017-10-13 21:58:15 +0530

    bug fix intro

--- src/components/introduction/writeIntroduction.vue
+++ src/components/introduction/writeIntroduction.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input v-model="introduction" type="text">
-        <button @click="checkIfUidIsLoaded">save</button>
+        <button @click="addIntroInDb">save</button>
     </div>
 </template>
 <script>
@@ -12,28 +12,44 @@
             }
         },
         methods:{
-            //checkIfUidIsLoaded
-            checkIfUidIsLoaded(){ //recursive
+            addIntroInDb(){
+                let vm = this
+                let schoolUid = this.$store.state.auth.user.uid
+
+                //console.log(schoolEmail)
+                this.$store.state.db.db.ref('introduction/' + schoolUid + '/' + 'introduction').set(vm.introduction)
+                this.$store.state.db.db.ref('introduction/' + schoolUid + '/' + 'schoolId').set(vm.$route.params.schoolId)
+            },
+            getIntroFromDb(){
+                let vm = this
+                let schoolUid = this.$store.state.auth.user.uid
+
+                this.$store.state.db.db.ref('introduction/' + schoolUid)
+                    .once('value', function(snapIntro){
+                        //console.log(snapIntro.val())
+                        //1 -> null
+                        if(snapIntro.val() == null){
+                            //do nothing
+                        }else { //2-> some value
+                            vm.introduction = snapIntro.val().introduction
+                        }
+                        //stop loader /*****************
+                    })
+            },
+            checkIfUidIsLoadedOnPageLoad(){ //recursive
+                //start loader /*****************
                 console.log('checkIfUidIsLoaded')
                 if(!this.$store.state.auth.isLoggedIn){ //stillNotLoggedIn
                     setTimeout(()=>{
                         this.checkIfUidIsLoaded() // call again after 1 sec
                     },1000)
                 }else{ //loggedIn
-                    this.addIntroInDb()
+                    this.getIntroFromDb()
                 }
             },
-            addIntroInDb(){
-                let vm = this
-               let schoolEmail = this.$store.state.auth.user.email
-                while(schoolEmail.indexOf('.') != -1)
-                    schoolEmail = schoolEmail.replace(".","replaceddothere")
-                //console.log(schoolEmail)
-                this.$store.state.db.db.ref('introduction/' + schoolEmail + '/' + 'introduction').set(vm.introduction)
-                this.$store.state.db.db.ref('introduction/' + schoolEmail + '/' + 'schoolId').set(vm.$route.params.schoolId)
-
-
-            }
+        },
+        created(){
+            this.checkIfUidIsLoadedOnPageLoad()
         }
     }
 </script>
